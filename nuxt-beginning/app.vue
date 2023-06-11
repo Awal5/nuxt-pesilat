012b6b72786f776407ca0f6c96c4dbf733543266
Rizki Awal R <rizkiawal16@gmail.com>
2023-06-11 23:27:41 +0700

    tugas todo list beres

--- nuxt-beginning/app.vue
+++ nuxt-beginning/app.vue
@@ -1,30 +1,42 @@
 <template>
-  <div class="list-task">
+  <div class="list-task p-4">
+    <h1>Todo List</h1>
     <div
       v-for="task of tasks"
+      :key="task.id"
       class="item-task d-flex align-items-start border-bottom pt-3 pb-4"
     >
       <input
         type="checkbox"
         name="status"
         id="task"
-        class="me-2 mt-2"
+        class="me-2 mt-2 form-check-input"
         :checked="task.isDone"
+        @change="toggleTaskCompletion(task)"
       />
       <div class="d-flex flex-column">
-        <div class="title-task mb-1">
-          {{ task.title }}
+        <div class="title-task mb-1 fw-bold">
+          <h2 :class="{ 'text-decoration-line-through': task.isDone }">
+            {{ task.title }}
+          </h2>
         </div>
-        <div class="description-task small text-muted">
+        <div class="description-task small text-muted fs-5">
           {{ task.description }}
         </div>
+        <button @click="deleteTask(task.id)" class="btn btn-danger mt-3">
+          Delete
+        </button>
       </div>
     </div>
+    <p v-if="tasks.length === 0">Belum ada Task</p>
     <div class="action py-2">
       <button
         class="add-button"
         v-if="!isCreating"
-        @click="isCreating = !isCreating"
+        @click="
+          emptyForm();
+          isCreating = !isCreating;
+        "
       >
         Add Task
       </button>
@@ -46,7 +58,15 @@
           </div>
         </div>
         <div class="button-wrapper d-flex">
-          <button class="btn btn-primary me-2" @click="submit">Save</button>
+          <button
+            class="btn btn-primary me-2"
+            @click="
+              submit();
+              isCreating = !isCreating;
+            "
+          >
+            Save
+          </button>
           <button
             class="btn btn-outline-secondary"
             @click="isCreating = !isCreating"
@@ -67,8 +87,9 @@ export default {
     return {
       tasks: [
         {
+          id: 1,
           title: "Task 1",
-          description: "ini deskripsi",
+          description: "Deskripsi 1",
           isDone: false,
         },
       ],
@@ -80,11 +101,22 @@ export default {
   methods: {
     submit() {
       this.tasks.push({
+        id: this.tasks.length + 1,
         title: this.titleValue,
         description: this.descriptionValue,
         isDone: false,
       });
       console.log(this.tasks);
+    },
+    emptyForm() {
+      this.titleValue = "";
+      this.descriptionValue = "";
+    },
+    deleteTask() {
+      this.tasks.splice(this.tasks.length - 1, 1);
+    },
+    toggleTaskCompletion(task) {
+      task.isDone = !task.isDone;
     },
   },
 };
